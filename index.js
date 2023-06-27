17a76657f073bbf50a64e11430e264b0e0466368
Billy Rennekamp <billy.rennekamp@gmail.com>
2023-06-27 13:49:11 +0200

    rename

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 const ViperABI = require("./ContractsAddress/ABI-sepolia-Viper.json");
-const ViperSepolia = require("./ContractsAddress/Viper-Sepolia.json");
+const ViperSepolia = require("./ContractsAddress/sepolia-Viper.json");
 
 const ControllerABI = require("./ContractsAddress/ABI-sepolia-Controller.json");
-const ControllerSepolia = require("./ContractsAddress/Controller-Sepolia.json");
+const ControllerSepolia = require("./ContractsAddress/sepolia-Controller.json");
 
 const MetadataABI = require("./ContractsAddress/ABI-sepolia-Metadata.json");
-const MetadataSepolia = require("./ContractsAddress/Metadata-Sepolia.json");
+const MetadataSepolia = require("./ContractsAddress/sepolia-Metadata.json");
 
 const BiteByViperABI = require("./ContractsAddress/ABI-sepolia-BiteByViper.json");
-const BiteByViperSepolia = require("./ContractsAddress/BiteByViper-Sepolia.json");
+const BiteByViperSepolia = require("./ContractsAddress/sepolia-BiteByViper.json");
 
 module.exports = {
   Viper: {
