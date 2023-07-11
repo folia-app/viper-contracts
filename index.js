173b5f25b1988b113a38ee1ecf5e5cc5afbabfee
Billy Rennekamp <billy.rennekamp@gmail.com>
2023-07-11 11:16:31 +0200

    added to index

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
-const ViperABI = require("./ContractsAddress/ABI-sepolia-Viper.json");
+const ViperABI = require("./ContractsAddress/ABI-homestead-Viper.json");
+const Viper = require("./ContractsAddress/homestead-Viper.json");
 const ViperSepolia = require("./ContractsAddress/sepolia-Viper.json");
 
-const MetadataABI = require("./ContractsAddress/ABI-sepolia-Metadata.json");
+const MetadataABI = require("./ContractsAddress/ABI-homestead-Metadata.json");
+const Metadata = require("./ContractsAddress/homestead-Metadata.json");
 const MetadataSepolia = require("./ContractsAddress/sepolia-Metadata.json");
 
-const BiteByViperABI = require("./ContractsAddress/ABI-sepolia-BiteByViper.json");
+const BiteByViperABI = require("./ContractsAddress/ABI-homestead-BiteByViper.json");
+const BiteByViper = require("./ContractsAddress/homestead-BiteByViper.json");
 const BiteByViperSepolia = require("./ContractsAddress/sepolia-BiteByViper.json");
 
 const { merkleAddresses } = require("./merkleAddresses.js");
@@ -15,22 +18,31 @@ module.exports = {
   Viper: {
     abi: ViperABI.abi,
     networks: {
+      '1': Viper,
+      'homestead': Viper,
+      'mainnet': Viper,
       '11155111': ViperSepolia,
-      'sepolia': ViperSepolia
+      'sepolia': ViperSepolia,
     },
   },
   Metadata: {
     abi: MetadataABI.abi,
     networks: {
+      '1': Metadata,
+      'homestead': Metadata,
+      'mainnet': Metadata,
       '11155111': MetadataSepolia,
-      'sepolia': MetadataSepolia
+      'sepolia': MetadataSepolia,
     },
   },
   BiteByViper: {
     abi: BiteByViperABI.abi,
     networks: {
+      '1': BiteByViper,
+      'homestead': BiteByViper,
+      'mainnet': BiteByViper,
       '11155111': BiteByViperSepolia,
-      'sepolia': BiteByViperSepolia
+      'sepolia': BiteByViperSepolia,
     },
   },
 }
